952354d8205480be26969ffcea3f9a344a01832f
OseasMelendez <oseasmelendes@gmail.com>
2025-02-04 09:32:05 -0600

    Metasapp layout

---
+++ src/componentes/compartidos/Pie.jsx
@@ -0,0 +1,8 @@
+function Pie(){
+    return(
+        <footer>
+            <small>Desarrollado y diseñado por Oseas Melendez. Todos los derechos reservados.©2025.</small>
+        </footer>
+    );
+}
+ export default Pie; 
---
+++ src/componentes/compartidos/Principal.jsx
@@ -0,0 +1,14 @@
+function Principal({children}){
+    return(
+        <div>
+            <sidenav>
+                <a href="/lista">Lista</a>
+                <a href="/crear">Crear</a>
+            </sidenav>
+            <main>
+                {children}
+            </main>
+        </div>
+    );
+}
+ export default Principal;
